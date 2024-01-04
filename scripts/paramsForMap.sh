760b103369247012df475a6c18383026687257e9
robocar <robocar-stash@ail-sv.com>
2024-01-04 10:13:03 -0800

    Thanh Le: - Chris modified foxtrot configuration - Thanh commited the latest paramsForMap.sh

--- scripts/paramsForMap.sh
+++ scripts/paramsForMap.sh
@@ -20,7 +20,7 @@ SanbornSantaClara() {
     echo "using map_name $map_name"
     rosparam set /map_name $map_name
     rosparam set SANBORN_CREATE_ANNOTATION_FILE 0
-    rosparam set USE_FASTER_FORMAT true
+    rosparam set USE_FASTER_FORMAT True
     rosparam set SANBORN_VERBOSE_ERROR_CHECKING 0
     rosparam set /siteFrame/originLat 37.397186956864289
     rosparam set /siteFrame/originLon -122.04398000000006
@@ -31,7 +31,6 @@ SanbornSantaClara() {
     rosparam set ycoordOffset 0.35
     rosparam set SANBORN_SLHACK_DX 0
     rosparam set SANBORN_SLHACK_DY 0
-    rosparam set USE_FASTER_FORMAT true
 }
 
 MiniMap() {
@@ -66,7 +65,8 @@ SanbornPNH() {
     echo $(rosparam set ycoordOffset 0.0)
     echo $(rosparam set SANBORN_SLHACK_DX 0)
     echo $(rosparam set SANBORN_SLHACK_DY 0)
-    rosparam set USE_FASTER_FORMAT false
+    #rosparam set USE_FASTER_FORMAT false
+    rosparam set USE_FASTER_FORMAT true
 }
 
 
@@ -105,7 +105,9 @@ ThunderHill() {
     echo $(rosparam set /siteFrame/originLon -122.04398000000006)  
     echo $(rosparam set /map_verbosityLevel 0)     
     echo $(rosparam set NO_SAVE 1)
-    rosparam set USE_FASTER_FORMAT false
+    rosparam set USE_FASTER_FORMAT true
+SIMINITIALPOSE="-24324.0,237437.0,0.787"
+rosparam set /simpulator_start_pose $SIMINITIALPOSE
 }
 
 LearnMapSantaClara() {
@@ -116,15 +118,15 @@ LearnMapSantaClara() {
     echo $(rosparam set NO_SAVE 1)
 
     # With learned data
-    #echo $(rosparam set /map_name "SC_Cached")
-    #echo $(rosparam set CREATE_ANNOTATION_FILE 0)     
-    #echo $(rosparam set /map_verbosityLevel 0)
+    echo $(rosparam set /map_name "SC_Cached")
+    echo $(rosparam set CREATE_ANNOTATION_FILE 0)     
+    echo $(rosparam set /map_verbosityLevel 0)
     
     # Without learned data
     #echo $(rosparam set /map_name "SC.set")
     #echo $(rosparam set CREATE_ANNOTATION_FILE 1)
     #echo $(rosparam set /map_verbosityLevel 10)     
-    rosparam set USE_FASTER_FORMAT false
+    rosparam set USE_FASTER_FORMAT true
 }
 
 LearnMapSanMiguel() {
@@ -169,9 +171,9 @@ LearnMapSF() {
 LearnMapFranklin() {
 
     # Common data
-    echo $(rosparam set /siteFrame/originLat 35.939640)
-    echo $(rosparam set /siteFrame/originLon -86.813031) 
-    echo $(rosparam set NO_SAVE 1)
+    #echo $(rosparam set /siteFrame/originLat 35.939640)
+    #echo $(rosparam set /siteFrame/originLon -86.813031) 
+    #echo $(rosparam set NO_SAVE 1)
     
     echo $(rosparam set /siteFrame/originLat 37.397186956864289)
     echo $(rosparam set /siteFrame/originLon -122.04398000000006)  
@@ -185,8 +187,133 @@ LearnMapFranklin() {
     # Without learned data
     echo $(rosparam set /map_name "Franklin.set")
     echo $(rosparam set CREATE_ANNOTATION_FILE 0)     
-    echo $(rosparam set /map_verbosityLevel 10)     
+    echo $(rosparam set /map_verbosityLevel 10)
+    rosparam set xcoordOffset 0.0
+    rosparam set ycoordOffset 0.0
     rosparam set USE_FASTER_FORMAT true
+
+SIMINITIALPOSE="4799.,-2245.,0.0"
+rosparam set /simpulator_start_pose $SIMINITIALPOSE
+}
+
+set_new_MM_map() {
+
+    echo "Setting up params for new_MM_map."
+    
+    # just set the passed in map name and go for it!
+    echo $(rosparam set /map_name $map_name)
+    
+    # TO-DOs: commented out as not seeing its effective yet.
+    originLat=36.00
+    originLon=139.833333
+    echo $(rosparam set /siteFrame/originLat $originLat)
+    echo $(rosparam set /siteFrame/originLon $originLon)
+    # echo $(rosparam set NO_SAVE 0)
+
+    # Without learned data
+    useFF=false
+    createAno=true
+    echo $(rosparam set AISAN_MM_CREATE_ANNOTATION_FILE 0)   
+    echo $(rosparam set CREATE_ANNOTATION_FILE $createAno)      
+    # echo $(rosparam set /map_verbosityLevel 10)     
+    
+    # use faster format is bool
+    echo $(rosparam set USE_FASTER_FORMAT $useFF)
+    
+
+    #echo $(rosparam get USE_FASTER_FORMAT)
+    #echo $(rosparam get CREATE_ANNOTATION_FILE)
+    
+    echo "Setting: /siteFrame/originLat $originLat"
+    echo "Setting: /siteFrame/originLon $originLon"
+    echo "Setting: CREATE_ANNOTATION_FILE $createAno"
+    echo "Setting: USE_FASTER_FORMAT $useFF"
+    
+}
+
+set_new_UK_map() {
+
+    echo "Setting up params for new_UK_map."
+    
+    # just set the passed in map name and go for it!
+    echo $(rosparam set /map_name $map_name)
+    
+    # TO-DOs: commented out as not seeing its effective yet.
+    originLat=51.501
+    originLon=-0.069
+    echo $(rosparam set /siteFrame/originLat $originLat)
+    echo $(rosparam set /siteFrame/originLon $originLon)  
+    # echo $(rosparam set NO_SAVE 0)
+
+    # Without learned data
+    useFF=false
+    createAno=true
+    echo $(rosparam set AISAN_MM_CREATE_ANNOTATION_FILE 0)   
+    echo $(rosparam set CREATE_ANNOTATION_FILE $createAno)      
+    # echo $(rosparam set /map_verbosityLevel 10)     
+    
+    # use faster format is bool
+    echo $(rosparam set USE_FASTER_FORMAT $useFF)
+    
+
+    #echo $(rosparam get USE_FASTER_FORMAT)
+    #echo $(rosparam get CREATE_ANNOTATION_FILE)
+    
+    echo "Setting: /siteFrame/originLat $originLat"
+    echo "Setting: /siteFrame/originLon $originLon"
+    echo "Setting: CREATE_ANNOTATION_FILE $createAno"
+    echo "Setting: USE_FASTER_FORMAT $useFF"
+}
+
+set_new_LONDON_map() {
+
+    echo "Setting up params for new_LONDON_map."
+    
+    # just set the passed in map name and go for it!
+    echo $(rosparam set /map_name $map_name)
+    
+    # TO-DOs: commented out as not seeing its effective yet.
+    originLat=51.371838681287009
+    originLon=-1.3107924391239296
+    echo $(rosparam set /siteFrame/originLat $originLat)
+    echo $(rosparam set /siteFrame/originLon $originLon)  
+    # echo $(rosparam set NO_SAVE 0)
+
+    # Without learned data
+    useFF=false
+    createAno=true
+    echo $(rosparam set AISAN_MM_CREATE_ANNOTATION_FILE 0)   
+    echo $(rosparam set CREATE_ANNOTATION_FILE $createAno)
+    echo $(rosparam set AISAN_UK_COEF_A 5.0)
+    echo $(rosparam set AISAN_UK_COEF_B 8.0)
+    # echo $(rosparam set /map_verbosityLevel 10)     
+    
+    # use faster format is bool
+    echo $(rosparam set USE_FASTER_FORMAT $useFF)
+    
+
+    #echo $(rosparam get USE_FASTER_FORMAT)
+    #echo $(rosparam get CREATE_ANNOTATION_FILE)
+    
+    echo "Setting: /siteFrame/originLat $originLat"
+    echo "Setting: /siteFrame/originLon $originLon"
+    echo "Setting: CREATE_ANNOTATION_FILE $createAno"
+    echo "Setting: USE_FASTER_FORMAT $useFF"
+}
+
+######################################################
+# a tiled map in Santa Clara
+SCTile() {
+echo "Setting map to SCTile."
+rosparam set /siteFrame/originLat 37.397186956864289
+rosparam set /siteFrame/originLon -122.04398000000006
+rosparam set /map_name SCTile
+rosparam set CREATE_ANNOTATION_FILE 0
+rosparam set USE_FASTER_FORMAT true
+
+# adding extra that seems to be required for tiled map
+SIMINITIALPOSE="4799.,-2245.,0.0"
+rosparam set /simpulator_start_pose $SIMINITIALPOSE
 }
         
 SetMapName() {
@@ -224,7 +351,11 @@ elif [ $map_name == "Noe.set" ];          then LearnMapSF;
 elif [ $map_name == "Franklin.set" ];     then LearnMapFranklin;
 elif [ $map_name == "THill_Cached" ];     then ThunderHill;
 elif [ $map_name == "Sanborn2020PNHv2" ]; then SanbornPNH;
-else                                           SetMapName;
+elif [ $map_name == "new_MM_map" ];       then set_new_MM_map;
+elif [ $map_name == "new_UK_map" ];       then set_new_UK_map;
+elif [ $map_name == "new_LONDON_map" ];   then set_new_LONDON_map;
+elif [ $map_name == "SCTile" ];           then SCTile;
+else                                      SetMapName;
 fi
 }
 
