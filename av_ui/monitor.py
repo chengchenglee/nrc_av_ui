ac46b5f77417f4b0b4ea4128c391461d8e3942e1
Chris Ostafew <christopher.ostafew@nissan-usa.com>
2024-08-16 13:03:37 -0700

    Parse node status and rate from diag message.

--- av_ui/monitor.py
+++ av_ui/monitor.py
@@ -26,9 +26,24 @@ class Monitor:
     self.msgCount = 0
     self.statusText = ""
     self.data = np.zeros(5)
+    self.nodeRate   = 0
+    self.nodeStatus = 0
 
   def setRates(self,rates):
     self.good, self.failing, self.failed = rates
+
+  def readStatusStr(self,statusStr):
+    lines = statusStr.split('\n')
+    data = []
+    for line in lines:
+      lineData = line.split(',')
+      data.append(list(lineData))
+      for i in range(len(self.data)):
+        if len(lineData) < 2: continue
+        if lineData[0] == 'r':
+          self.nodeRate = float(lineData[1])
+        elif lineData[0] == 's':
+          self.nodeStatus = int(lineData[1])
 
   def msgCallback(self, data):
     current_time = time.time()
@@ -61,6 +76,7 @@ class Monitor:
     elif "health" in self.topic:
       self.customLedValue = -1
       for diagStatus in data.status:
+        self.readStatusStr(diagStatus.message)
         if diagStatus.level == 4 or diagStatus.level == 5:
           self.customLedValue = max(self.customLedValue, diagStatus.level)
     
@@ -69,16 +85,33 @@ class Monitor:
     tDiffFailed = min(5, 3*(1/self.failed))
     current_time = time.time()
     tDiff = min(10, max(0.005, current_time-self.tLastRvcd))
-    rate = min(200, max(0, 1/self.avgTimeDiff))
     
-    self.msgText = "MsgCount: " + str(self.msgCount) + ", Rate: " + str(round(rate,2))+", "+self.statusText
+    # Node is self reporting status
+    if self.nodeRate > 0 and self.nodeStatus > 0:
+      tDiffFailing = 1.1
+      tDiffFailed = 2.5
+      rate = min(200, max(0, 1/self.avgTimeDiff))
+      self.msgText = "Msgs: " + str(self.msgCount) + ", H-Rate: "+str(round(rate,2))+", AlgRate: " + str(round(self.nodeRate,2))+" "+self.statusText
+      
+      healthMsgStatus = 2
+      if tDiff > tDiffFailed or self.avgTimeDiff >= tDiffFailed:
+        healthMsgStatus = 1
+      elif tDiff < tDiffFailing and self.avgTimeDiff < tDiffFailing:
+        healthMsgStatus = 3
+      
+      self.status = min(self.nodeStatus,healthMsgStatus)
     
-    if tDiff > tDiffFailed or self.avgTimeDiff >= tDiffFailed:
-      self.status = 1
-    elif tDiff < tDiffFailing and self.avgTimeDiff < tDiffFailing:
-      self.status = 3
+    # Determine rate and status from health message rate
     else:
-      self.status = 2
+      rate = min(200, max(0, 1/self.avgTimeDiff))
+      self.msgText = "MsgCount: " + str(self.msgCount) + ", Rate: " + str(round(rate,2))+", "+self.statusText
+    
+      if tDiff > tDiffFailed or self.avgTimeDiff >= tDiffFailed:
+        self.status = 1
+      elif tDiff < tDiffFailing and self.avgTimeDiff < tDiffFailing:
+        self.status = 3
+      else:
+        self.status = 2
 
     if self.status <= 1:
       if isStarted == 0:
