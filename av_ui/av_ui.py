3ab06446917d89755de4efdf020671b5e1160f94
poza <poza@ail-sv.com>
2024-09-09 14:01:06 -0700

    setting rosparam for fvla

--- av_ui/av_ui.py
+++ av_ui/av_ui.py
@@ -60,6 +60,7 @@ if True:
     os.system("rosrun nrc_svcs paramsForDriving.sh")
     os.system("rosrun nrc_svcs paramsForMap.sh "+agent.mapName)
     os.system("rosparam set /robot_description -t "+rospkg.RosPack().get_path('nrc_av_ui')+'/calib/'+agent.agentUrdf+'.urdf')
+  if True:
     os.system("rosparam set /agent_name "+agent.name)
     os.system("rosparam set /agent_config "+agent_config)
 
