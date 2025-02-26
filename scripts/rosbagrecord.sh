33b00e6b81a59f9df0b8b780bbedab295ca67158
robocar <robocar-stash@ail-sv.com>
2025-02-25 16:50:08 -0800

    allow option to assign different directory for rosbag recording

--- scripts/rosbagrecord.sh
+++ scripts/rosbagrecord.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
-if [ -z ${NRCSV_BAG_DIR+x} ]; then
-  NRC_BAG_DIR=/opt/data/rosbag;
+NRC_BAG_DIR=$(rosparam get NRCSV_ROSBAG_RECORD_DIR)
+if [ -z ${NRC_BAG_DIR+x} ]; then
+  NRC_BAG_DIR=/opt/data/rosbag/;
 fi
+echo "NRC_BAG_DIR is ${NRC_BAG_DIR}"
 d=`date +%F`
 cd ${NRC_BAG_DIR}
 mkdir -p ${d}
 cd ${d}
-echo "Recording to directory: " $PWD
+echo "=================Recording to directory: " $PWD
 rosbag record "$@" --split --duration=3m --buffsize=1024
