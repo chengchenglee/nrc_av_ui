e6397ff14f7359d6c442b7e5b900a4e1bbbec371
Chris Ostafew <christopher.ostafew@nissan-usa.com>
2025-05-14 18:58:19 -0700

    Send eporo image from AvAgent when no camera stream.

--- av_ui/avagent.py
+++ av_ui/avagent.py
@@ -59,7 +59,8 @@ class AvAgent:
     self.wmStatus = WmStatus()
     self.passThroughWm  = False
     self.passThroughImg = False
-    self.enableSendDebugImg = False
+    self.enableSendDebugImg = True
+    self.debugImgIdx = 1
     self.remoteWmDisplayOn = 0
     self.remoteWmDisplayLastReq = 0
     self.remoteMonTeleoping = 0
@@ -320,7 +321,9 @@ class AvAgent:
       
   def sendDebugImg(self):
     # Create ros message to send
-    pathToDefaultImg = os.path.expanduser('~')+'/projects/nrc_ws/src/nrc_av_ui/av_ui/test1.png'
+    pathToDefaultImg = os.path.expanduser('~')+'/projects/nrc_ws/src/nrc_av_ui/av_ui/eporo'+str(self.debugImgIdx)+'.jpg'
+    self.debugImgIdx = self.debugImgIdx + 1;
+    if self.debugImgIdx > 4: self.debugImgIdx = 1
     with Image.open(pathToDefaultImg) as img:
         # Convert image to RGB (JPEG doesn't support transparency)
         rgb_img = img.convert('RGB')
